418a84cd0d4f56e12786a67571c81effdc549984
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-01-03 19:00:50 +0600

    chore(favicon): remove unused favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,14 @@ import "./globals.css";
 import Providers from "@app/Providers";
 import { cookies } from "next/headers";
 
-const FAVICON_SIZES = [16, 32, 48, 96, 128, 256];
-
 export const metadata: Metadata = {
   title: "IDK",
   description: "IDK app",
-  icons: FAVICON_SIZES.map((size) => {
-    return {
-      rel: "icon",
-      type: "image/png",
-      sizes: `${size}x${size}`,
-      url: `/favicon-${size}.png`,
-    };
-  }),
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: "/favicon.svg",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default async function RootLayout({
@@ -27,10 +22,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={theme?.value === "light" ? "light" : "dark"}>
-      <head>
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head>
       <body className={`font-mono antialiased`}>
         <Providers>{children}</Providers>
       </body>
